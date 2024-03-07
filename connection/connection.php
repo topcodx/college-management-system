3fcffe168da996f8ad4efb79c4edf79438fc7b2a
Vidhi <Vidhi@gmail.com>
2024-03-07 11:13:40 +0530

    change configue file

--- connection/connection.php
+++ connection/connection.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-	$con=mysqli_connect("localhost","root","root","college-mg-system");
+	$con=mysqli_connect("localhost","root","","college");
 	if(!$con)
 	{
 		echo "Connection is not Successfully";
